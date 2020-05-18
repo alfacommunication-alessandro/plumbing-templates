528325ba49dec7746ea1304807ac7f5b2000f427
alfaa <alessandro@alfacommunication.it>
2020-05-18 11:44:52 +0200

    Support for child themes

--- src/functions.php
+++ src/functions.php
@@ -61,6 +61,10 @@ function set_template_directory(string $path, string $post_type = 'page')
  */
 function get_wordpress_template_directory($post_type)
 {
+    if (is_child_theme) {
+        return apply_filters('template-dir/theme-directory', get_stylesheet_directory(), $post_type);
+    }
+    
     return apply_filters('template-dir/theme-directory', get_template_directory(), $post_type);
 }
 
